38e9b8ee78e290c1e43047b6c259a6e1ea7c07fe
Latch <lachlan.scott@gmail.com>
2020-02-24 20:05:54 +0900

    Solutions for class exercises

---
+++ TableView/Exercises.swift
@@ -0,0 +1,24 @@
+//
+//  Exercises.swift
+//  TableView
+//
+//  Created by Lachlan Scott on 2020/02/24.
+//  Copyright © 2020 Fudoshin Design. All rights reserved.
+//
+
+// This file includes code answers to exercises in the ViewController class
+// Try to work out your own answers before referring to these!
+
+//! Exercise: User feedback
+//! 1) if there's no more data to delete disable the removeItemButton
+//! 2) then provide some way of re-enabling it
+/*
+	if(tableData.count == 0){
+		removeButton.isEnabled = false
+		barTitle.title = "No Data!"
+	}
+*/
+
+// ------
+
+
